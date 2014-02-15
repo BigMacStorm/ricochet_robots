8be9e6324f67ece765d7d5668b8c63c5f949a942
David <DavidRayVaughan00@gmail.com>
2014-02-14 19:08:38 -0800

    random crap

--- robot.cpp
+++ robot.cpp
@@ -7,6 +7,12 @@ using namespace std;
 
 int main()
 {
+    int x, y, z;
+    cout << "hello world!" << endl;
+    
+    cin >> x >> y >> z;
+    
+    cout << "good job" << endl;
     
     return 0;
 }
