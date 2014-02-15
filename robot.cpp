790bd613d4ebf87175bd736fcfd7d009cc19f9c3
David <DavidRayVaughan00@gmail.com>
2014-02-14 20:02:35 -0800

    cell struct

--- robot.cpp
+++ robot.cpp
@@ -7,19 +7,24 @@ using namespace std;
 
 // robot struct
     // x and y and color
+
+struct cell
+{
+    bool occupied;
+    bool robot, left, up, right, down;
+};
     
 // Functions
     // move
     // read in maps
     // display
     // check if win
+    // populate board
 
 int main()
 {
     // Read the map(s)
     // Create all objects
-    
-    
     // Loop 1 start
         // Randomly choose goal
         // Loop start
